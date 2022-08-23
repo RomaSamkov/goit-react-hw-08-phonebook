2ba058d8092515a66283463a22798cf322f8e97f
RomaSamkov <92638707+RomaSamkov@users.noreply.github.com>
2022-08-23 19:21:23 +0000

    Deploying to gh-pages from @ RomaSamkov/goit-react-hw-08-phonebook@1b1ba14fd4eb94196e55fd9a7b2fc7e7ac99a9ee 🚀

---
+++ static/js/223.5aad96b8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[223],{9223:function(e,t,n){n.r(t),n.d(t,{default:function(){return d}});var r,c,o,s=n(168),a=n(8789),h=(0,a.ZP)("div")(r||(r=(0,s.Z)([""]))),l=(0,a.ZP)("h1")(c||(c=(0,s.Z)([""]))),u=(0,a.ZP)("p")(o||(o=(0,s.Z)([""]))),i=n(184);function d(){return(0,i.jsx)(i.Fragment,{children:(0,i.jsxs)(h,{children:[(0,i.jsx)(l,{children:"Welcome to ContactsBook"}),(0,i.jsx)(u,{children:"Text ................."})]})})}}}]);
+//# sourceMappingURL=223.5aad96b8.chunk.js.map

b271eaaf829663b080651c6d5a19ec64780f3633
RomaSamkov <Romansamkov.rs@gmail.com>
2022-08-24 17:34:09 +0300

    Update Header.styled.jsx

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -7,11 +7,16 @@ export const Container = styled('div')`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  min-height: 45px;
+  min-height: 50px;
   padding: 20px;
+  padding-left: 80px;
+  padding-right: 80px;
   box-shadow: 0px 10px 21px -2px rgba(0, 0, 0, 0.5);
   -webkit-box-shadow: 0px 10px 21px -2px rgba(0, 0, 0, 0.5);
   -moz-box-shadow: 0px 10px 21px -2px rgba(0, 0, 0, 0.5);
+  /* -moz-box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.5);
+  -webkit-box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.5);
+  box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.5); */
 `;
 
 export const StyledLink = styled(Link)`
@@ -19,5 +24,14 @@ export const StyledLink = styled(Link)`
 `;
 
 export const Title = styled('h2')`
+  display: block;
+  padding: 10px 25px;
+  border-radius: 25px;
   margin: 0;
+  color: #0d6efd;
+  background-color: rgb(221, 216, 236);
+  &:hover,
+  :focus {
+    color: #fff;
+  }
 `;

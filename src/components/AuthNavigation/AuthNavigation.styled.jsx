17a9fe39d3b3bd75115a491e71d0eb9431756dbd
RomaSamkov <Romansamkov.rs@gmail.com>
2022-08-24 17:14:24 +0300

    Update AuthNavigation.styled.jsx

--- src/components/AuthNavigation/AuthNavigation.styled.jsx
+++ src/components/AuthNavigation/AuthNavigation.styled.jsx
@@ -2,9 +2,24 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const StyledLink = styled(NavLink)`
-  
+  display: flex;
+  justify-content: center;
+  padding: 0.5rem 1rem;
+  text-decoration: none;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out;
+  color: #0d6efd;
+  background-color: rgb(221, 216, 236);
+  border-radius: 25px;
+  width: 80px;
+  &:hover,
+  :focus {
+    color: #fff;
+  }
 `;
 
 export const LinkWraper = styled('div')`
-  
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 20px;
 `;
